3298922ebe89bef4f3ab37df7adea3a0991a3dcf
unknown <harler.david@gmail.com>
2023-06-14 12:40:05 -0700

    performed one-way t-tests analyzing means of suspension coil PSI collectively and across each individual lot.

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -8,3 +8,9 @@ summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_cleara
 SuspensionCoil <- read.csv(file='suspension_coil.csv',check.names=F,stringsAsFactors = F)
 total_summary <- SuspensionCoil %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = sd(PSI)^2, SD = sd(PSI), .groups = 'keep') #create summary table with multiple columns
 lot_summary <- SuspensionCoil %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = sd(PSI)^2, SD = sd(PSI), .groups = 'keep') #create summary table with multiple columns
+
+# Deliverable 3
+t.test(SuspensionCoil$PSI, mu = mean(SuspensionCoil$PSI))
+t.test(subset(SuspensionCoil$PSI,SuspensionCoil$Manufacturing_Lot == "Lot1"), mu=mean(SuspensionCoil$PSI))
+t.test(subset(SuspensionCoil$PSI,SuspensionCoil$Manufacturing_Lot == "Lot2"), mu=mean(SuspensionCoil$PSI))
+t.test(subset(SuspensionCoil$PSI,SuspensionCoil$Manufacturing_Lot == "Lot3"), mu=mean(SuspensionCoil$PSI))
