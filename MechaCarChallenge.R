f82650c08193148010520e023aa9b8e4854fac0e
unknown <harler.david@gmail.com>
2023-06-14 12:09:56 -0700

    created summary statistics for suspension coil PSI.

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -5,3 +5,5 @@ summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_cleara
 
 
 SuspensionCoil <- read.csv(file='suspension_coil.csv',check.names=F,stringsAsFactors = F)
+total_summary <- SuspensionCoil %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = sd(PSI)^2, SD = sd(PSI), .groups = 'keep') #create summary table with multiple columns
+lot_summary <- SuspensionCoil %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = sd(PSI)^2, SD = sd(PSI), .groups = 'keep') #create summary table with multiple columns
